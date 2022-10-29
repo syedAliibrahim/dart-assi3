5b4ccb2947bd0079dbd9e57d0f63c4c49f8bea43
ali <syedaliibrahim50@gmail.com>
2022-10-29 10:34:16 +0500

    q1

---
+++ app.dart
@@ -0,0 +1,12 @@
+void main() {
+//q#1   
+ var list1 = ["ali","abuzar","salman","dell","dart","shan","ibrahim"];
+  print(list1[0]);
+  print(list1[1]);
+  print(list1[2]);
+  print(list1[3]);
+  print(list1[4]);
+  print(list1[5]);
+  print(list1[6]);
+  
+}
